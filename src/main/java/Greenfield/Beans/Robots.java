74ddc018afb5348bbccd1899d1f42aeeb8b933f3
Matteo Rigat <matteoluigi.rigat@mail.polimi.it>
2023-05-15 17:48:36 +0200

    district topic fix

--- src/main/java/Greenfield/Beans/Robots.java
+++ src/main/java/Greenfield/Beans/Robots.java
@@ -14,11 +14,16 @@ public class Robots {
 
     @XmlElement(name="Greenfield_robots")
     private List<Robot> robotslist;
+    private int[] numOfRobots;
 
     private static Robots instance;
 
     private Robots() {
         robotslist = new ArrayList<Robot>();
+
+        numOfRobots = new int[4];
+        for(int i=0; i<4; i++)
+            numOfRobots[i] = 0;
     }
 
     //singleton
@@ -44,16 +49,14 @@ public class Robots {
 
     public synchronized boolean removeById(int id){
         System.out.println("\n--> ROBOT REMOVED - " + id);
+        removeFromDistrictById(id);
         return robotslist.removeIf(r -> r.getId() == id);
     }
 
-    public int getDistrictById(int id){
+    private void removeFromDistrictById(int id){
         for(Robot r : robotslist)
             if(r.getId() == id)
-                return calculateDistrict(r.getPosition().x, r.getPosition().y);
-
-        return -1;
-
+                numOfRobots[calculateDistrict(r.getPosition().x, r.getPosition().y)]--;
     }
 
     private int calculateDistrict(int x, int y){
@@ -67,6 +70,57 @@ public class Robots {
             return 3;
 
         return -1;
+    }
+
+
+    public Robot.Coordinates generatePosition(Robot r){
+
+        int min, max;
+        int district = 0;
+        int x,y;
+
+        for (int i = 0; i < numOfRobots.length; i++) {
+            System.out.println(i + " " + numOfRobots[i]);
+            if (numOfRobots[i] < numOfRobots[district]) {
+                district = i;
+            }
+        }
+
+        System.out.println("\n+1 robot on district " + district);
+        System.out.println(numOfRobots[district]);
+        numOfRobots[district]++;
+        System.out.println(numOfRobots[district]);
+
+        switch (district){
+            case 0: case 3:
+                min = 0;
+                max = 4;
+                break;
+            case 1: case 2:
+                min = 5;
+                max = 9;
+                break;
+            default: return null;
+        }
+
+        x = (int)(min + (Math.random()*(max - min + 1)));
+
+        switch (district){
+            case 0: case 1:
+                min = 0;
+                max = 4;
+                break;
+            case 2: case 3:
+                min = 5;
+                max = 9;
+                break;
+            default: return null;
+        }
+
+        y = (int)(Math.floor(Math.random() *(max - min + 1) + min));
+
+        r.setPosition(x, y);
+        return r.getPosition();
     }
 
 }
--- src/main/java/Greenfield/Services/RobotsService.java
+++ src/main/java/Greenfield/Services/RobotsService.java
@@ -11,8 +11,6 @@ import javax.ws.rs.core.Response;
 @Path("robots")
 public class RobotsService {
 
-    private int[] numOfRobots = null;
-
     //private static Map<Integer,Robot> robots = new HashMap<Integer,Robot>();
 
     //GET: The list of the cleaning robots currently located in Greenfield
@@ -28,7 +26,7 @@ public class RobotsService {
     @Consumes({"application/json", "application/xml"})
     public Response addRobot(Robot r){
         if(Robots.getInstance().add(r)){
-            RobotResponseData robotResponseData = new RobotResponseData(generatePosition(r), Robots.getInstance());
+            RobotResponseData robotResponseData = new RobotResponseData(Robots.getInstance().generatePosition(r), Robots.getInstance());
             return Response.ok(robotResponseData).type("application/json").build();
         }
         else return Response.status(Response.Status.CONFLICT).build();
@@ -41,68 +39,12 @@ public class RobotsService {
     @Consumes({"application/json", "application/xml"})
     public Response deleteRobot(@PathParam("id") int id){
         if(Robots.getInstance().removeById(id)){
-            int i = Robots.getInstance().getDistrictById(id);
-            if(i > 0){
-                numOfRobots[i]--;
-                System.out.println("\n-1 robot on district " + i);
-            }
-
             return Response.ok().type("application/json").build();
         }
         else return Response.status(Response.Status.NOT_MODIFIED).build();
     }
 
 
-    private Robot.Coordinates generatePosition(Robot r){
-        if(numOfRobots == null){
-            numOfRobots = new int[4];
-            for(int i=0; i<4; i++)
-                numOfRobots[i] = 0;
-        }
-        int min = numOfRobots[0];
-        int max;
-        int district = 0;
-        int x,y;
 
-        for(int i=1; i<4; i++)
-            if(min > numOfRobots[i]){
-                min = numOfRobots[i];
-                district = i;
-            }
-
-        System.out.println("\n+1 robot on district " + district);
-        numOfRobots[district]++;
-
-        switch (district){
-            case 0: case 3:
-                min = 0;
-                max = 4;
-                break;
-            case 1: case 2:
-                min = 5;
-                max = 9;
-                break;
-            default: return null;
-        }
-
-        x = (int)(min + (Math.random()*(max - min + 1)));
-
-        switch (district){
-            case 0: case 1:
-                min = 0;
-                max = 4;
-                break;
-            case 2: case 3:
-                min = 5;
-                max = 9;
-                break;
-            default: return null;
-        }
-
-        y = (int)(Math.floor(Math.random() *(max - min + 1) + min));
-
-        r.setPosition(x, y);
-        return r.getPosition();
-    }
 
 }
